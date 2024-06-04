079a4ad07fa617d8cfe159e8f10f5ced33fd3655
Sifiso Myeza <ext.sifiso.myeza@mobisys.de>
2024-06-04 08:29:07 +0200

    Fix form error state

--- src/components/BeveragesForm/BeveragesForm.vue
+++ src/components/BeveragesForm/BeveragesForm.vue
@@ -3,6 +3,13 @@
     <header>
       <h1>{{ title }}</h1>
     </header>
+    <v-alert
+      v-if="errorMessage.length > 0"
+      :text="errorMessage"
+      color="error"
+      variant="outlined"
+      border
+    ></v-alert>
     <form>
       <v-text-field
         type="text"
@@ -18,21 +25,18 @@
         label="How many beers?"
         variant="outlined"
         v-model.number="beerQty"
-        :rules="[rules.required]"
       ></v-text-field>
       <v-text-field
         type="number"
         label="how many ciders?"
         variant="outlined"
         v-model.number="ciderQty"
-        :rules="[rules.required]"
       ></v-text-field>
       <v-text-field
         type="number"
         label="How many premixes?"
         variant="outlined"
         v-model.number="premixQty"
-        :rules="[rules.required]"
       ></v-text-field>
       <v-btn class="button" variant="outlined" color="blue" @click="addNewTab">
         {{ buttonText }}
@@ -59,15 +63,24 @@ const tabOwner = ref(owner || '')
 const beerQty = ref(0)
 const ciderQty = ref(0)
 const premixQty = ref(0)
+const errorMessage = ref('')
 
 const rules = computed(() => ({
   required: (value: string) => !!value || 'Required'
 }))
 
 function addNewTab() {
-  if (!tabOwner.value || (beerQty.value === 0 && ciderQty.value === 0 && premixQty.value === 0)) {
+  if (!tabOwner.value) {
+    errorMessage.value = "The tab owner's name is required!"
     return
   }
+
+  if (beerQty.value === 0 && ciderQty.value === 0 && premixQty.value === 0) {
+    errorMessage.value = 'At least one beverage is required!'
+    return
+  }
+
+  errorMessage.value = ''
 
   const order: OrderedBeverageData[] = beverages.map((beverage) => {
     switch (beverage.beverage) {
